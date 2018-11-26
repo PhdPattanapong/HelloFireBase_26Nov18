8dd7ab650298b1f2b9137c4d0c79ce23343b13ad
Phai <pattanapong.th@hotmail.com>
2018-11-26 14:11:45 +0700

    Connected Firebase

--- HelloFirebase/HelloFirebase.ino
+++ HelloFirebase/HelloFirebase.ino
@@ -1,10 +1,15 @@
-#include <ESP8266WiFi.h>
 
+#include <FirebaseArduino.h>
+#include <ESP8266WiFi.h>
 
 
 //Config Connected Router
 #define WiFi_SSID "MastreEWTC_2.4G"
 #define WiFi_Password "12345abcde"
+
+// Config Connected Data Base
+#define Firebase_Host "greenhouse-3073d.firebaseio.com"
+#define Firebase_Auth "Wogx5BtFbmZ02JGlC8y83BGUlBjX0A8YORUNAOuX"
 
 // Explicit
 int countInt = 0;
@@ -26,7 +31,9 @@ void setup()
     Serial.println();
     Serial.print("Sucess Connected : ");
     Serial.println(WiFi.localIP());
-
+   
+//    Start Connected Firebase
+    Firebase.begin(Firebase_Host, Firebase_Auth);
 
 
 } // Setup
@@ -38,6 +45,11 @@ void loop() {
 
   Serial.print(" CountInt ==> " );
   Serial.println(countInt);
+
+  // Post Value To FireBase
+
+  Firebase.setInt("TestNumner", countInt);
+  
   delay(1000);
 
 } // loop 
